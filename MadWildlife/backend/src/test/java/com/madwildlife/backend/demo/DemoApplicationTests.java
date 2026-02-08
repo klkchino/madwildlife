0e2761465cbf05e7e596ce8aa6302227a447e0e1
layz <kkidau@wisc.edu>
2026-02-08 12:17:04 -0600

    unit testing user creation and user log creation

--- MadWildlife/backend/src/test/java/com/madwildlife/backend/demo/DemoApplicationTests.java
+++ MadWildlife/backend/src/test/java/com/madwildlife/backend/demo/DemoApplicationTests.java
@@ -10,4 +10,27 @@ class DemoApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void testDocumentCreation() {
+		
+		DocumentCreationFirebase docCreator = new DocumentCreationFirebase();
+		String userId = docCreator.userUpdate("Test User", "test@example.com");
+		if (userId != null) {
+			System.out.println("Created user with ID: " + userId);
+		} else {
+			System.out.println("Failed to create user.");
+		}
+	}
+
+	@Test
+	void testUserLogCreation() {
+		DocumentCreationFirebase docCreator = new DocumentCreationFirebase();
+		String userLogId = docCreator.userlogUpdate("userId123", "speciesId456", 
+											"Saw a rare bird!", "http://example.com/photo.jpg", 37.7749, -122.4194);
+		if (userLogId != null) {
+			System.out.println("Created user log with ID: " + userLogId);
+		} else {
+			System.out.println("Failed to create user log.");
+		}
+	}
 }
