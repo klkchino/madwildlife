b90dc03a28d062222231643a8e5059d5879eb826
Oscar Zapata <oscar@Oscars-MacBook-Air-2.local>
2026-02-07 19:17:51 -0600

    catch up

--- MadWildlife/components/map.tsx
+++ MadWildlife/components/map.tsx
@@ -2,17 +2,46 @@
 import React from "react";
 import { StyleSheet } from "react-native";
 import MapView from "react-native-maps";
+import { PROVIDER_GOOGLE } from "react-native-maps";
+import { useEffect, useRef } from "react";
+import * as Location from "expo-location";
 
 export const Map = () => {
+  const mapRef = useRef<MapView | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      mapRef.current?.animateToRegion(
+        {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        },
+        800
+      );
+    })();
+  }, []);
+
   return (
     <MapView
       style={styles.map}
+      ref={mapRef}
       initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+        latitude: 43.084283,
+        longitude: -89.428786,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
       }}
+       provider={PROVIDER_GOOGLE}
+       showsUserLocation
     />
   );
 };
